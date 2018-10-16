e82abae6f02bc1b15f09b65c28ef7a86dbb7af47
Kat Gavin <marikathryn@gmail.com>
2018-10-16 17:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase === string) {
+  if (string === string.toLowerCase()) {
+    var lowerCase = string.toLowerCase();
     return 'I can\'t hear you!'
   }
     if (string.toUpperCase() === string) {
